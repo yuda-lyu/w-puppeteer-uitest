06e6bac17cba388bae88a6060d03845ed046c076
yuda-lyu <firsemisphere@gmail.com>
2020-03-17 20:18:41 +0800

    modify: update pks

--- dist/readJson.umd.js
+++ dist/readJson.umd.js
@@ -1,7 +1,7 @@
 /*!
- * readJson v1.0.31
+ * readJson v1.0.32
  * (c) 2018-2019 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
-!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?module.exports=n(require("fs"),require("json5")):"function"==typeof define&&define.amd?define(["fs","json5"],n):(e=e||self).readJson=n(e.fs,e.json5)}(this,(function(e,n){"use strict";return e=e&&e.hasOwnProperty("default")?e.default:e,n=n&&n.hasOwnProperty("default")?n.default:n,function(t){var f=e.readFileSync(t,"utf8");return n.parse(f)}}));
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t(require("fs"),require("json5")):"function"==typeof define&&define.amd?define(["fs","json5"],t):(e=e||self).readJson=t(e.fs,e.json5)}(this,(function(e,t){"use strict";return e=e&&Object.prototype.hasOwnProperty.call(e,"default")?e.default:e,t=t&&Object.prototype.hasOwnProperty.call(t,"default")?t.default:t,function(n){var o=e.readFileSync(n,"utf8");return t.parse(o)}}));
 //# sourceMappingURL=readJson.umd.js.map
