85582a44d35674b71c7d5585459906a8527e11a3
yuda-lyu <firsemisphere@gmail.com>
2023-03-29 15:09:37 +0800

    modify: update pks

--- dist/readJson.umd.js
+++ dist/readJson.umd.js
@@ -1,7 +1,7 @@
 /*!
- * readJson v1.0.37
+ * readJson v1.0.38
  * (c) 2018-2021 yuda-lyu(semisphere)
  * Released under the MIT License.
  */
-!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?module.exports=n(require("fs"),require("json5")):"function"==typeof define&&define.amd?define(["fs","json5"],n):(e="undefined"!=typeof globalThis?globalThis:e||self).readJson=n(e.fs,e.json5)}(this,(function(e,n){"use strict";function t(e){return e&&"object"==typeof e&&"default"in e?e:{default:e}}var f=t(e),o=t(n);return function(e){var n=f.default.readFileSync(e,"utf8");return o.default.parse(n)}}));
+!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?module.exports=n(require("fs"),require("json5")):"function"==typeof define&&define.amd?define(["fs","json5"],n):(e="undefined"!=typeof globalThis?globalThis:e||self).readJson=n(e.fs,e.json5)}(this,(function(e,n){"use strict";return function(o){let f=e.readFileSync(o,"utf8");return n.parse(f)}}));
 //# sourceMappingURL=readJson.umd.js.map
